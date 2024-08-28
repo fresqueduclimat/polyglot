2d7bb41c8e481364ad3f596ebfd1cba48ce05132
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-08-28 11:02:04 +0200

    embed pdf in view

--- app/controllers/document_controller.rb
+++ app/controllers/document_controller.rb
@@ -11,8 +11,9 @@ class DocumentController < ApplicationController
 
     @documents = Documents::Mapping.new(documents:).call
     @result = Tolgee::ContentFetcher.new(document_id:, language:, api_key:).call
-    @img_files = Dir.glob(File.join("public/#{selected_document[:config_name].underscore}", "*.png"))
-    @document_size = selected_document[:page_size]
+    # @img_files = Dir.glob(File.join("public/#{selected_document[:config_name].underscore}", "*.png")) # for rendering images
+    # @document_size = selected_document[:page_size] # for rendering images
+    @pdf_file_path = "pdf/#{selected_document[:config_name].underscore}_#{language}.pdf" # for rendering generated pdf
 
     return if document_id == 8.to_s
 
