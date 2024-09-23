0ca132827836572f132a816b6e99266cabe504cf
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-09-23 10:49:50 +0200

    Refactor config modules to remove repetitive 'page' keys

--- app/classes/documents/polyglot_fdc_adult_cards_images.rb
+++ app/classes/documents/polyglot_fdc_adult_cards_images.rb
@@ -1,7 +1,7 @@
-module PolyglotFdcAdultCardsImages
+module Documents::PolyglotFdcAdultCardsImages
   BASE_PATH = "public/images/polyglot_fdc_adult_cards".freeze
   CONFIG = {
-    "0": {
+    1 => {
       IMG01: {
         "fr-FR" => {url: "#{BASE_PATH}/logo-fr.png", x_pos: 25, y_pos: 60, scale: 0.25},
         "ar-AR" => {url: "#{BASE_PATH}/logo-ar.png", x_pos: 25, y_pos: 60, scale: 0.3}
