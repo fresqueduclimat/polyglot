d92668f1ed19595be81a64a42001ca657ae9ef89
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-09-27 14:19:35 +0200

    Add mission_control-jobs gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount MissionControl::Jobs::Engine, at: "/jobs"
+
   root "document#show"
   resource :document, only: [:show], controller: "document"
   get "up" => "rails/health#show", as: :rails_health_check
