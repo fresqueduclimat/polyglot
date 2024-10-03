72481e81d2ba305c94f8952038644dadee76b4c3
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-10-03 14:44:51 +0200

    Add Brevo SMTP settings in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,14 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV.fetch("SMTP_HOST", "smtp-relay.brevo.com"),
+    port: ENV.fetch("SMTP_PORT", "587"),
+    authentication: :plain,
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    enable_starttls_auto: true
+  }
 end
