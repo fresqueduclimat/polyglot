800d8da0f41acd532df8f9e7b76366946ca6b4e4
Julien CAFFET <julien@allianceblock.io>
2024-10-14 10:47:14 +0200

    Update CI/CD

--- infra/scripts/startup_polyglot.sh
+++ infra/scripts/startup_polyglot.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 set -e
 
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /app/tmp/pids/server.pid
+
+# Execute a db migrate only when ENVIRONMENT var is set (only on AWS)
+# if [ -z ${ENVIRONMENT+x} ]; then 
+#     bundle exec rake db:migrate
+# fi
+
+# Execute db migrate only on staging and prod
+if [ "${ENVIRONMENT}" == "staging" ] || [ "${ENVIRONMENT}" == "prod" ]; then
+    bundle exec rake db:migrate
+fi
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
