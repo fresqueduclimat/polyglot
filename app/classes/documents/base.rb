6ac1fa330bc6409a580a690aa3f4cd0ec93a0ee1
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-12-05 13:02:30 +0100

    Comment out unused documents and update name of the Facilitation Guide

--- app/classes/documents/base.rb
+++ app/classes/documents/base.rb
@@ -1,33 +1,33 @@
 module Documents::Base
-  FDC_ADULT_CARDS = {
-    config_name: "FdcAdultCards",
-    document_id: 7,
-    name: "Adult cards",
-    page_size: [597, 408],
-    api_key: ENV["TOLGEE_FDC_ADULT_CARDS"]
-  }.freeze
+  # FDC_ADULT_CARDS = {
+  #   config_name: "FdcAdultCards",
+  #   document_id: 7,
+  #   name: "Adult cards",
+  #   page_size: [597, 408],
+  #   api_key: ENV["TOLGEE_FDC_ADULT_CARDS"]
+  # }.freeze
 
   FACILITATION_GUIDE = {
     config_name: "FacilitationGuide",
     document_id: 22,
-    name: "Facilitation Guide",
+    name: "New Fresker's Guide",
     page_size: [595, 842],
     api_key: ENV["TOLGEE_FACILITATION_GUIDE"]
   }.freeze
 
-  FACILITATION_KIT = {
-    config_name: "FacilitationKit",
-    document_id: 24,
-    name: "Facilitation Kit",
-    page_size: [960, 540],
-    api_key: ENV["TOLGEE_FACILITATION_KIT"]
-  }.freeze
+  # FACILITATION_KIT = {
+  #   config_name: "FacilitationKit",
+  #   document_id: 24,
+  #   name: "Facilitation Kit",
+  #   page_size: [960, 540],
+  #   api_key: ENV["TOLGEE_FACILITATION_KIT"]
+  # }.freeze
 
-  KIDS_CARDS = {
-    config_name: "KidsCards",
-    document_id: 8,
-    name: "Kids Cards",
-    page_size: [597, 408],
-    api_key: ENV["TOLGEE_FDC_KIDS_CARDS"]
-  }.freeze
+  # KIDS_CARDS = {
+  #   config_name: "KidsCards",
+  #   document_id: 8,
+  #   name: "Kids Cards",
+  #   page_size: [597, 408],
+  #   api_key: ENV["TOLGEE_FDC_KIDS_CARDS"]
+  # }.freeze
 end
